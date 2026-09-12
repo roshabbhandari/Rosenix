f8306028def1f5d33ee71bef4644916c05a5aa97
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-09-12 20:26:20 +0545

    feat: add task cancellation token

---
+++ rosenix/cancellation.py
@@ -0,0 +1,14 @@
+class CancellationToken:
+    def __init__(self):
+        self._cancelled = False
+
+    @property
+    def cancelled(self) -> bool:
+        return self._cancelled
+
+    def cancel(self) -> None:
+        self._cancelled = True
+
+    def raise_if_cancelled(self) -> None:
+        if self._cancelled:
+            raise RuntimeError("operation cancelled")

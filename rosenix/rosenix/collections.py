f5d7139f519fc29daeaf209dd8e58cfe9047cdbb
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-09-13 20:27:02 +0545

    feat: add collection chunking helper

---
+++ rosenix/rosenix/collections.py
@@ -0,0 +1,5 @@
+def chunked(items, size):
+    if size <= 0:
+        raise ValueError("size must be positive")
+    values = list(items)
+    return [values[index:index + size] for index in range(0, len(values), size)]
